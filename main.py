dd9bb44321c962891ddfbcf521ac8c56e77b7504
Glegos93 <50709521+Glegos93@users.noreply.github.com>
2023-10-20 12:35:34 -0500

    feat: connected to discord

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import os
-import random
 import discord
+from dotenv import load_dotenv
+load_dotenv()
 
+
+TOKEN = os.environ['DISCORD_TOKEN']
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+client.run(TOKEN)
