d67130d0cc7bcb76ef4caa03fbcf10e23098e6d6
Glegos93 <50709521+Glegos93@users.noreply.github.com>
2023-11-06 14:03:15 -0600

    fix: updated discord.py
    mightve introduced some rate issues

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import discord
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 
 TOKEN = os.environ['DISCORD_TOKEN']
 
@@ -22,19 +22,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+
 @client.event
 async def on_message(message):
+    # test only
     if message.content.startswith('!test'):
         await message.channel.send("it worked")
     if message.content.startswith("!join"):
-        role = discord.utils.get(message.guild.roles, name = "Participant")
+        role = discord.utils.get(message.guild.roles, name="Participant")
+        if role in message.author.roles:
+            await message.channel.send("You are already participating")
+            return
         await message.author.add_roles(role)
         await message.channel.send("Welcome to the season!")
     if message.content.startswith('!reset'):
         if "Admin" not in [role.name for role in message.author.roles]:
             await message.channel.send("You do not have permission to use this command.")
             return
-        role = discord.utils.get(message.guild.roles, name = "Participant")
+        role = discord.utils.get(message.guild.roles, name="Participant")
         for member in message.guild.members:
             if role in member.roles:
                 await member.remove_roles(role)
@@ -42,5 +47,3 @@ async def on_message(message):
 
 
 client.run(TOKEN)
-
-
